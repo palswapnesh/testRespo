ee67e3a59d049b85d5efaf2e9060a05a57812846
swapnesh <swapnesh@DESKTOP-5EHFTMT>
2019-01-01 23:18:49 +0530

    ddd
    ddd

---
+++ TokenWebAPI/TokenWebAPI/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using TokenWebAPI.Models;
+
+namespace TokenWebAPI.Controllers
+{
+    public class TestController : ApiController
+    {
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public IHttpActionResult Register(RegisterBindingModel model)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+       // [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public IHttpActionResult GetUsers()
+        {
+            angularTokenApiEntities entities = new angularTokenApiEntities();
+            var data = entities.Users.ToList();
+            return Ok(data);
+        }
+
+    }
+}
